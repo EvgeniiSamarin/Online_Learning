588af7b847869bb0876be1aa8c428a5b1850956f
SamarinEvgenii <57903146+SamarinEvgenii@users.noreply.github.com>
2020-04-26 00:23:17 +0400

    StudentYaml
    Student serialization to YAML

---
+++ Yaml_Student/src/main/java/StudentYamlDecorator.java
@@ -0,0 +1,94 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentYamlDecorator extends OutputStream {
+    private DataOutputStream outputStream;
+
+    public StudentYamlDecorator(OutputStream outputStream) {
+        this.outputStream = new DataOutputStream(new BufferedOutputStream(outputStream));
+    }
+
+    public void write(int b) throws IOException {
+        outputStream.write(b);
+    }
+
+    public void write(byte[] b, int off, int len) throws IOException {
+        outputStream.write(b, off, len);
+    }
+
+    public void flush() throws IOException {
+        outputStream.flush();
+    }
+
+    public void writeBoolean(boolean v) throws IOException {
+        outputStream.writeBoolean(v);
+    }
+
+    public void writeByte(int v) throws IOException {
+        outputStream.writeByte(v);
+    }
+
+    public void writeShort(int v) throws IOException {
+        outputStream.writeShort(v);
+    }
+
+    public void writeChar(int v) throws IOException {
+        outputStream.writeChar(v);
+    }
+
+    public void writeInt(int v) throws IOException {
+        outputStream.writeInt(v);
+    }
+
+    public void writeLong(long v) throws IOException {
+        outputStream.writeLong(v);
+    }
+
+    public void writeFloat(float v) throws IOException {
+        outputStream.writeFloat(v);
+    }
+
+    public void writeDouble(double v) throws IOException {
+        outputStream.writeDouble(v);
+    }
+
+    public void writeBytes(String s) throws IOException {
+        outputStream.writeBytes(s);
+    }
+
+    public void writeChars(String s) throws IOException {
+        outputStream.writeChars(s);
+    }
+
+    public void writeUTF(String str) throws IOException {
+        outputStream.writeUTF(str);
+    }
+
+    public int size() {
+        return outputStream.size();
+    }
+
+    public void write(byte[] b) throws IOException {
+        outputStream.write(b);
+    }
+
+    public void close() throws IOException {
+        outputStream.close();
+    }
+    public void writeStudentYaml(Student student) throws IOException {
+        ObjectMapper om = new ObjectMapper(new YAMLFactory());
+        om.writeValue((OutputStream) outputStream, student);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Student student = new Student("Евгений", (byte)28, "11-903", 'm');
+        StudentYamlDecorator studentYamlDecorator = new StudentYamlDecorator(new FileOutputStream("Student.yaml"));
+        studentYamlDecorator.writeStudentYaml(student);
+    }
+}
